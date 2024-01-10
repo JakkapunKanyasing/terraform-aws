ffa4fd650cdf501465e29a96c4586f11d96ddec2
JakkapunKanyasing <kcristiano7@gmail.com>
2024-01-10 10:42:12 +0700

    edit main.tf

--- connect-aws/main.tf
+++ connect-aws/main.tf
@@ -1,22 +1,24 @@
-# Connect by credentials file
-# provider "aws" {
-#  region                  = var.region
-#  profile                 = "jakkapun-kanyasing"
-  # if use credentials authen
-  # note: Do not set the path to the credentials file, in order to use the default AWS credentials.
-  # shared_credentials_file = "~/.aws/credentials"
-#}
-# Connect by hardcode
-#provider "aws" {
-#  region                  = var.region
-#  profile                 = "jakkapun-kanyasing"
-#  access_key              = "your_aws_access_key"
-#  secret_key              = "your_aws_secret_access_key"
-#}
-# Connect by Terminal Environment Variables
+## Connect by credentials file
+# if use credentials authen
+# note: Do not set the path to the credentials file, in order to use the default AWS credentials.
+# shared_credentials_file = "~/.aws/credentials"
+       provider "aws" {
+            region                  = var.region
+            profile                 = "jakkapun-kanyasing"
+       }
+
+## Connect by hardcode
+      # provider "aws" {
+          #  region                  = var.region
+          #  access_key              = "your_aws_access_key"
+          #  secret_key              = "your_aws_secret_access_key"
+      # }
+
+
+## Connect by Terminal Environment Variables
 # You can create these environment variables in your current terminal session by executing the following commands:
 # export AWS_ACCESS_KEY_ID=your_aws_access_key
 # export AWS_SECRET_ACCESS_KEY=your_aws_secret_access_key
-provider "aws" {
-  region                  = var.region
-}
+      # provider "aws" {
+          #  region                  = var.region
+      # }
