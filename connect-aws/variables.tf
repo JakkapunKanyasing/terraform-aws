adabd60483737c9ed7fede44b9d17879a51645ae
JakkapunKanyasing <kcristiano7@gmail.com>
2024-01-10 10:51:34 +0700

    edit file

--- connect-aws/variables.tf
+++ connect-aws/variables.tf
@@ -2,33 +2,3 @@ variable "region" {
   type = string
   default = "ap-southeast-1"
 }
-variable "create" {
-  description = "Whether to create an instance"
-  type        = bool
-  default     = true
-}
-variable "ami" {
-  description = "ID of AMI to use for the instance"
-  type        = string
-  default     = null
-}
-variable "instance_type" {
-  description = "The type of instance to start"
-  type        = string
-  default     = "t2.micro"
-}
-variable "ignore_ami_changes" {
-  description = "Whether changes to the AMI ID changes should be ignored by Terraform. Note - changing this value will result in the replacement of the instance"
-  type        = bool
-  default     = false
-}
-variable "create_spot_instance" {
-  description = "Depicts if the instance is a spot instance"
-  type        = bool
-  default     = false
-}
-variable "ami_ssm_parameter" {
-  description = "SSM parameter name for the AMI ID. For Amazon Linux AMI SSM parameters see [reference](https://docs.aws.amazon.com/systems-manager/latest/userguide/parameter-store-public-parameters-ami.html)"
-  type        = string
-  default     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-}
